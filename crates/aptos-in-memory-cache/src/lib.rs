c83f8b7a4f024d2674d95fd78d64c48ffc33f95a
Justin Chang <justin@aptoslabs.com>
2024-02-29 16:54:02 -0800

    add documentation

--- crates/aptos-in-memory-cache/src/lib.rs
+++ crates/aptos-in-memory-cache/src/lib.rs
@@ -11,6 +11,9 @@ where
     K: Eq + Hash + Clone,
     V: Clone,
 {
+    /// Get the value for a given key. Return [`None`] if the key is not in the cache.
     fn get(&self, key: &K) -> Option<V>;
+
+    /// Inserts a given key-value pair in cache. Panics if the insert fails.
     fn insert(&mut self, key: K, value: V);
 }
